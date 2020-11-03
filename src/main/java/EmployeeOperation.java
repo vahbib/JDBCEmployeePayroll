4d7dd70415ff53c2a7fcd0d33857927d289e30fd
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-03 17:20:29 +0530

    Update details

--- src/main/java/EmployeeOperation.java
+++ src/main/java/EmployeeOperation.java
@@ -1,2 +1,51 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class EmployeeOperation {
+    static List<Employee> employee_list = new ArrayList<>();
+
+    public void readData(Connection con) throws JDBCCustomException {
+        try{
+            String query = "select * from employee_payroll";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()){
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                double salary = rs.getDouble(3);
+                Date date = rs.getDate(4);
+
+                System.out.print("\nID: " + id);
+                System.out.print("\nName: " + name);
+                System.out.print("\nSalary: " + salary);
+                System.out.println("\nDate: " + date);
+
+                Employee emp = new Employee(id,name,salary,date);
+                employee_list.add(emp);
+            }
+        }catch(Exception e){
+            throw new JDBCCustomException("Read Process Unsuccessful");
+        }
+    }
+    public void updateData(Connection con, String column, String name, String value) throws JDBCCustomException, SQLException {
+        try{
+            String query = String.format("Update employee_payroll set " + column +" = " + value +" where Name = '"+ name + "'");
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(query);
+            for(Employee e: EmployeeOperation.employee_list) {
+                if (e.getName().equals(name)) {
+                    e.setSalary(Double.parseDouble(value));
+                    System.out.println(e.getSalary());
+                }
+            }
+        }catch(Exception e){
+            throw new JDBCCustomException("Update Process Unsuccessful");
+        }
+    }
 }
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,2 +1,40 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class TestClass {
+    EmployeeOperation eo;
+    DBConnection dbc;
+    Connection con;
+
+    @Before
+    public void init(){
+        eo = new EmployeeOperation();
+        dbc = new DBConnection();
+        con = dbc.getConnection();
+    }
+
+    @Test
+    public void onUpdation_compareEmpPayrollObjectWithDB() throws JDBCCustomException, SQLException {
+
+        /* UC3 -- update employee object and in the database and compare */
+        eo.readData(con);
+        eo.updateData(con, "Salary", "Terisa", "400000");
+
+        Employee e = null;
+        for(Employee ep: EmployeeOperation.employee_list){
+            if(ep.getName().equals("Terisa"))
+                e = ep;
+        }
+        ResultSet rs = con.createStatement().executeQuery("Select Salary from employee_payroll where Name = 'Terisa'");
+        double salary = 0;
+        while(rs.next()){
+            salary = rs.getDouble("Salary");
+        }
+        Assert.assertEquals(e.getSalary(), salary,0);
+    }
 }
