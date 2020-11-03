ccbfee4c18b9db79a2ee5fe2cb7847e37ec88299
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-03 21:16:48 +0530

    Retrive data between dates

--- src/main/java/EmployeeOperation.java
+++ src/main/java/EmployeeOperation.java
@@ -1,2 +1,63 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class EmployeeOperation {
+    static List<Employee> employee_list = new ArrayList<>();
+    private PreparedStatement preparedStatement;
+    private static EmployeeOperation employeeOperation;
+
+    public List<Employee> readData(Connection con) throws JDBCCustomException {
+        try{
+            String query = "select * from employee_payroll";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()){
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                double salary = rs.getDouble(3);
+                Date date = rs.getDate(4);
+
+                System.out.print("\nID: " + id);
+                System.out.print("\nName: " + name);
+                System.out.print("\nSalary: " + salary);
+                System.out.println("\nDate: " + date);
+
+                Employee emp = new Employee(id,name,salary,date);
+                employee_list.add(emp);
+            }
+        }catch(Exception e){
+            throw new JDBCCustomException("Read Process Unsuccessful");
+        }
+        return employee_list;
+    }
+    public void updateData(Connection con, String column, String name, String value) throws JDBCCustomException, SQLException {
+        try{
+            PreparedStatement stmt = con.prepareStatement("Update employee_payroll set Salary = ? where Name = ?");
+            stmt.setDouble(1,Double.parseDouble(value));
+            stmt.setString(2,name);
+            stmt.executeUpdate();
+            for(Employee e: EmployeeOperation.employee_list) {
+                if (e.getName().equals(name)) {
+                    e.setSalary(Double.parseDouble(value));
+                    System.out.println(e.getSalary());
+                }
+            }
+        }catch(Exception e){
+            throw new JDBCCustomException("Update Process Unsuccessful");
+        }
+    }
+    public List<Employee> retrieveEmployeesByDate(Connection con, String startDate, String endDate) throws SQLException, JDBCCustomException {
+        PreparedStatement stmt =con.prepareStatement("Select * from employee_payroll where StartDate between ? and ?");
+        stmt.setString(1, startDate);
+        stmt.setString(2,endDate);
+
+        stmt.executeQuery();
+
+        System.out.println(stmt.toString());
+
+        return employee_list;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws JDBCCustomException, SQLException {
+        DBConnection db = new DBConnection();
+        Connection con = db.getConnection();
+
+        EmployeeOperation employeeOperation = new EmployeeOperation();
+        employeeOperation.readData(con);
+        employeeOperation.retrieveEmployeesByDate(con,"2018-10-10", "2020-01-01");
+
+
+    }
 }
