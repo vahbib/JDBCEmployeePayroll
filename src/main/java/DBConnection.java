b8e9e2a1b530e40534b32d967453eafb64c512aa
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-03 15:44:42 +0530

    Ability to create payroll service database and have java program connect to database

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,2 +1,37 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class DBConnection {
+    public Connection getConnection(){
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "Lolalit";
+
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded..");
+        }catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath", e);
+        }
+
+        listDrivers();
+        try {
+            System.out.println("Connecting to the Database... " + jdbcURL);
+            con = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection was successful !!");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+    public static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while(driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println( "  " + driverClass.getClass().getName());
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to JDBC Program");
+        DBConnection db = new DBConnection();
+        Connection con = db.getConnection();
+        System.out.println(con);
+    }
 }
