dc1951e2dada640edf1e152c85ca25225ca90c6b
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-03 18:56:25 +0530

    Update details : Prepared Statement

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,2 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
+    public Connection getConnection(){
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "Lolalit";
+
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded..");
+        }catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath", e);
+        }
+        try {
+            System.out.println("Connecting to the Database... " + jdbcURL);
+            con = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection was successful !!");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args) throws JDBCCustomException {
+        DBConnection db = new DBConnection();
+        Connection con = db.getConnection();
+
+        EmployeeOperation employeeOperation = new EmployeeOperation();
+        employeeOperation.readData(con);
+
+
+    }
 }
