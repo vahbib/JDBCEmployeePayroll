d5872d222c07c42ed8403c50a7a70813f4ccea2f
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-05 19:18:56 +0530

    Ability to also add to payroll details when a new Employee is added to the Payroll

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class Main {
@@ -8,9 +9,9 @@ public class Main {
 
         EmployeeOperation employeeOperation = new EmployeeOperation();
         employeeOperation.readData(con);
-        employeeOperation.retrieveEmployeesByDate(con, "2018-10-10", "2020-01-01");
-        employeeOperation.dataBaseFunctionOps(con, "F");
-
-
+//        employeeOperation.retrieveEmployeesByDate(con, "2018-10-10", "2020-01-01");
+//        employeeOperation.dataBaseFunctionOps(con, "F");
+//        Date date = new Date(2016, 8, 25);
+//        employeeOperation.insertDataToEmployeeDB(con, "Fayaz", 'M', 300000, date);
     }
 }
