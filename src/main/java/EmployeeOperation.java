72933a8f584a8c12f5751fd1df621d84141fcd3a
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-05 21:25:09 +0530

    Implement the complete ER Diagram in the Database

--- src/main/java/EmployeeOperation.java
+++ src/main/java/EmployeeOperation.java
@@ -1,4 +1,3 @@
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,7 +8,11 @@ public class EmployeeOperation {
     private PreparedStatement preparedStatement;
     private static EmployeeOperation employeeOperation;
 
-    public List<Employee> readData(Connection con) throws JDBCCustomException {
+    public static List<Employee> getEmployee_list() {
+        return employee_list;
+    }
+
+    public List<Employee> readData(Connection con) throws JDBCCustomException, SQLException {
         try{
             String query = "select * from employee_payroll";
             Statement stmt = con.createStatement();
@@ -18,21 +21,24 @@ public class EmployeeOperation {
             while (rs.next()){
                 int id = rs.getInt(1);
                 String name = rs.getString(2);
-                String Gender = rs.getString(3);
+                char gender = rs.getString(3).charAt(0);
                 double salary = rs.getDouble(4);
                 Date date = rs.getDate(5);
 
                 System.out.print("\nID: " + id);
                 System.out.print("\nName: " + name);
-                System.out.print("\nGender: "+ Gender);
+                System.out.print("\nGender: "+ gender);
                 System.out.print("\nSalary: " + salary);
                 System.out.println("\nDate: " + date);
 
-                Employee emp = new Employee(id,name,salary,date);
+                Employee emp = new Employee(id,name,gender,salary,date);
                 employee_list.add(emp);
             }
         }catch(Exception e){
             throw new JDBCCustomException("Read Process Unsuccessful");
+        }
+        finally {
+            con.close();
         }
         return employee_list;
     }
@@ -49,7 +55,14 @@ public class EmployeeOperation {
                 }
             }
         }catch(Exception e){
+            try {
+                con.rollback();
+            } catch (SQLException sq) {
+                sq.printStackTrace();
+            }
             throw new JDBCCustomException("Update Process Unsuccessful");
+        }finally {
+            con.close();
         }
     }
     public List<Employee> retrieveEmployeesByDate(Connection con, String startDate, String endDate) throws SQLException, JDBCCustomException {
@@ -62,17 +75,87 @@ public class EmployeeOperation {
 
             System.out.println(stmt.toString());
         } catch (Exception e) {
+            try {
+                con.rollback();
+            } catch (SQLException sq) {
+                sq.printStackTrace();
+            }
             throw new JDBCCustomException("Retrieve by Date process Unsuccessful");
+        }
+        finally {
+            con.close();
         }
 
         return employee_list;
     }
     public List<Employee> dataBaseFunctionOps(Connection con, String gender) throws SQLException, JDBCCustomException {
-        PreparedStatement stmt =con.prepareStatement("select gender, count(gender), min(salary), max(salary), avg(salary), sum(salary) from employee_payroll group by ?");
-        stmt.setString(1, gender);
+        try {
+            PreparedStatement stmt = con.prepareStatement("select gender, count(gender), min(salary), max(salary), avg(salary), sum(salary) from employee_payroll group by ?");
+            stmt.setString(1, gender);
 
-        stmt.executeQuery();
+            stmt.executeQuery();
+        } catch (SQLException e) {
+            try {
+                con.rollback();
+            } catch (SQLException sq){
+                sq.printStackTrace();
+            }
+            throw new JDBCCustomException("DataBase Function operation Failed");
+        }
+        finally {
+            con.close();
+        }
 
         return employee_list;
+    }
+
+    public int insertDataToEmployeeDB(Connection con, String name, char gender, double salary, Date date) throws SQLException {
+        int result_query1 = -1, result_query2 = -1, result = 0;
+        int id = 0;
+        Employee emp = null;
+        try {
+            DBConnection jdbc_con = new DBConnection();
+            con = jdbc_con.getConnection();
+            String query = String.format("Insert into employee_payroll (Name,Gender,Salary,StartDate) values " +
+                    "('%s', '%s', '%s', '%s')", name, gender, salary, date);
+            Statement stmt = con.createStatement();
+            result_query1 = stmt.executeUpdate(query, stmt.RETURN_GENERATED_KEYS);
+            if (result_query1 == 1) {
+                ResultSet rs = stmt.getGeneratedKeys();
+                while (rs.next()) {
+                    id = rs.getInt(1);
+                    emp = new Employee(id, name, gender, salary, date);
+                }
+            }
+
+            double deductions = .2 * salary;
+            double taxable_pay = salary - deductions;
+            double income_tax = 0.1 * taxable_pay;
+            double net_salary = salary - income_tax;
+            String query2 = String.format("Insert into payroll_details (Id, basic_pay, deductions, taxable_pay, income_tax, netPay)" +
+                    " values ('%s', '%s', '%s', '%s', '%s', '%s')", id, salary, deductions, taxable_pay, income_tax, net_salary);
+            result_query2 = stmt.executeUpdate(query2, stmt.RETURN_GENERATED_KEYS);
+
+            if (result_query1 == 1 && result_query2 == 2) {
+                con.commit();
+                result = 1;
+                EmployeeOperation.getEmployee_list().add(emp);
+            }
+        } catch (SQLException sq) {
+            sq.printStackTrace();
+            try {
+                con.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class Main {
@@ -6,11 +7,15 @@ public class Main {
         DBConnection db = new DBConnection();
         Connection con = db.getConnection();
 
-        EmployeeOperation employeeOperation = new EmployeeOperation();
-        employeeOperation.readData(con);
-        employeeOperation.retrieveEmployeesByDate(con, "2018-10-10", "2020-01-01");
-        employeeOperation.dataBaseFunctionOps(con, "F");
-
+//        EmployeeOperation employeeOperation = new EmployeeOperation();
+//        employeeOperation.readData(con);
+//        employeeOperation.retrieveEmployeesByDate(con, "2018-10-10", "2020-01-01");
+//        employeeOperation.dataBaseFunctionOps(con, "F");
+//        Date date = new Date(2016, 8, 25);
+//        employeeOperation.insertDataToEmployeeDB(con, "Fayaz", 'M', 300000, date);
+        for (Employee e : EmployeeOperation.getEmployee_list()) {
+            System.out.println(e.toString());
+        }
 
     }
 }
